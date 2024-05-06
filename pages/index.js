96cc8136de1cadd5d60eb50c6c5ec89fd3f6484f
Alireza Asghari <aa9967316@gmail.com>
2024-05-06 08:46:58 +0330

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <h1>
+      D:\Code\nextjs-app\Todo-app-fullstack\node_modules
+      Home page
+    </h1>
+  );
+}
