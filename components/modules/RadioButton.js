441179204f610218627893fe303bfdd886edb71e
Alireza Asghari <aa9967316@gmail.com>
2024-05-07 09:53:17 +0330

    Create the add todo page

---
+++ components/modules/RadioButton.js
@@ -0,0 +1,29 @@
+const RadioButton = ({
+  name,
+  title,
+  icon,
+  radioChangeHandler,
+  rdaioState,
+  theme,
+}) => {
+  return (
+    <label
+      htmlFor={name}
+      className={`p-2 bg-[${theme}] min-w-[120px] max-w-[140px] rounded-md flex justify-between my-2 md:cursor-pointer [&[id=""]]`}
+    >
+      <span className="flex items-center">
+        {icon} <span className="px-1">{title}</span>
+      </span>
+      <input
+        type="radio"
+        value={name}
+        id={name}
+        name="todo-radio"
+        onChange={radioChangeHandler}
+        checked={rdaioState === name}
+      />
+    </label>
+  );
+};
+
+export default RadioButton;
---
+++ components/templates/AddTodoPage.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { LuListTodo } from "react-icons/lu";
+import RadioButton from "../modules/RadioButton";
+
+const AddTodoPage = () => {
+  const [title, setTitle] = useState("");
+  const [radio, setRadio] = useState("todo");
+
+  const radioChangeHandler = (e) => {
+    setRadio(e.target.value);
+  };
+
+  const addTodoHandler = () => {
+    console.log({radio, title})
+  };
+
+  return (
+    <div>
+      <h3 className="text-xl font-bold p-3">Add todo</h3>
+
+      <div className="m-3 mt-5">
+        <input
+          className="p-1 px-2 outline-none rounded-md"
+          type="text"
+          name="add-todo"
+          placeholder="todo title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <div className="mt-5 text-white ">
+          <RadioButton
+            name="todo"
+            title="Todo"
+            rdaioState={radio}
+            radioChangeHandler={radioChangeHandler}
+            icon={<LuListTodo />}
+            theme="#1679AB"
+          />
+          <RadioButton
+            name="inProgress"
+            title="in Progress"
+            rdaioState={radio}
+            radioChangeHandler={radioChangeHandler}
+            icon={<LuListTodo />}
+            theme="#1679AB"
+          />
+          <RadioButton
+            name="review"
+            title="review"
+            rdaioState={radio}
+            radioChangeHandler={radioChangeHandler}
+            icon={<LuListTodo />}
+            theme="#1679AB"
+          />
+          <RadioButton
+            name="done"
+            title="done"
+            rdaioState={radio}
+            radioChangeHandler={radioChangeHandler}
+            icon={<LuListTodo />}
+            theme="#535C91"
+          />
+        </div>
+
+        <button onClick={addTodoHandler} className="text-white p-1 px-4 my-6 bg-[#1679AB] rounded-md">Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTodoPage;
