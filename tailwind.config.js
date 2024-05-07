2618e6b35a221deb77a2f6d1346153988c6aec06
Alireza Asghari <aa9967316@gmail.com>
2024-05-07 19:25:50 +0330

    Create colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        todo: '#E65C19',
+        inProgress: '#F3CA52',
+        review: '#5AB2FF',
+        done: '#535C91',
+      }
     },
   },
   plugins: [],
